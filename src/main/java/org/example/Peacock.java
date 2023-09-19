3f496d39bd37047ade826b559e014f665eb19b5b
John <ashton.basile@gmail.com>
2023-09-19 11:08:43 +0100

    I wrote a Peacock class and a test of its colors

---
+++ src/main/java/org/example/Peacock.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Peacock {
+    private final String[] colors;
+
+    public Peacock() {
+        this.colors = new String[] {"Brown", "Green", "Yellow", "Blue"};
+    }
+
+    public String[] getColors() {
+        return colors;
+    }
+}
---
+++ src/test/java/ArrayTest.java
@@ -0,0 +1,17 @@
+import org.example.Peacock;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ArrayTest {
+    Peacock peacock;
+    @Before
+    public void before() {
+        peacock = new Peacock();
+    }
+
+    @Test
+    public void hasFourColors(){
+        assertEquals(4, peacock.getColors().length);
+    }
+}
