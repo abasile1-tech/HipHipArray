3e1d2ad93206689a50ff68d5955e71a4b0fa20b0
John <ashton.basile@gmail.com>
2023-09-19 12:02:44 +0100

    One type of for loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[3];
-        System.out.println(numbers[0]);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(7);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
     }
 }
